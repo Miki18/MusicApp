6fb35f26e5d231d31168ad34885bebee3c68019e
Miki18 <mikiadam24@wp.pl>
2025-05-20 17:51:12 +0200

    Refactoring

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from dearpygui.dearpygui import add_button
 #song class - each instance of this class stores 1 song
 class Song:
     def __init__(self, name, Title, Author, Year):
+        #Music properties
         self.filename = name
         self.Title = Title
         self.Author = Author
@@ -64,12 +65,14 @@ class Song:
             self.paused_time = (time.time() - self.start_time) + self.paused_time
             self.is_playing = False
 
+    #change volume
     def louder(self, sedner=None, app_data=None):
         self.volume = min(self.volume + 0.1, 1.0)
 
     def quieter(self, sedner=None, app_data=None):
         self.volume = max(self.volume - 0.1, 0.0)
 
+    #change speed
     def faster(self, sender=None, app_data=None):
         self.speed = min(self.speed + 0.1, 2.0)
         self.need_reload = True
@@ -78,6 +81,7 @@ class Song:
         self.speed = max(self.speed - 0.1, 0.1)
         self.need_reload = True
 
+    #skip
     def forward(self, sender=None, app_data=None):
         was_playing = False
         if self.is_playing:
@@ -100,6 +104,7 @@ class Song:
         if was_playing:
             self.play()
 
+    #reset
     def reset(self, sender=None, app_data=None):
         if self.is_playing:
             self.stop()
@@ -112,6 +117,7 @@ class Song:
                 round(wf.getnframes() / wf.getframerate(), 2))
             dpg.set_value("wave_info", info_text)
 
+    #get functions
     def getFilename(self, sender=None, app_data=None):
         return self.filename
 
@@ -124,6 +130,7 @@ class Song:
     def getYear(self, sender=None, app_data=None):
         return self.Year
 
+#global variables for music management
 SongList = []
 SongNumber = 0
 
@@ -160,41 +167,28 @@ def import_file_interract(sender, app_data):
     if file_path:
         import_file(file_path, "", "Unknown", "----")
 
+# function for buttons from song list
 def ChooseSong(k):
     global SongNumber
     SongNumber = k
     updateSongProperties()
 
+# create a callback for buttons from song list
 def create_callback(index):
     return lambda: ChooseSong(index)
 
+# Song list
 def update_song_list():
     dpg.delete_item("SongListWindow", children_only=True)
 
     for index, song in enumerate(SongList):
-        print("check: " + str(index))
         dpg.add_button(
             label=song.getTitle(),
-            callback=create_callback(index),
+            callback=create_callback(index),   # every button has a callback function with proper index
             parent="SongListWindow",
             width=200,
             height=50
         )
-
-# cut audio
-def cut_audio(input_file, output_file, start_time, end_time):
-    with wave.open(input_file, 'rb') as wf:
-        params = wf.getparams()
-        framerate = params.framerate
-        start_frame = int(start_time * framerate)
-        end_frame = int(end_time * framerate)
-
-        wf.setpos(start_frame)
-        frames = wf.readframes(end_frame - start_frame)
-
-    with wave.open(output_file, 'wb') as wf_out:
-        wf_out.setparams(params)
-        wf_out.writeframes(frames)
 
 #save files
 def save():
@@ -208,6 +202,7 @@ def save():
             file.write(Song.getAuthor() + "\n")
             file.write(Song.getYear() + "\n")
 
+#load files
 def Load(file_name, title, author, year):
     destination_folder = os.getcwd()
     destination_path = os.path.join(destination_folder, file_name)
@@ -278,7 +273,7 @@ if __name__ == "__main__":
     dpg.create_viewport(title="Music Program", width=1600, height=900, resizable=False, decorated=False)
 
     #Main Window
-    with dpg.window(label="Main Window", width=1600, height=900, tag="MainWindow", no_move=True, no_resize=True):
+    with dpg.window(label="Music App", width=1600, height=900, tag="MainWindow", no_move=True, no_resize=True):
         # Main section
         with dpg.group(horizontal=True):
 
@@ -309,7 +304,7 @@ if __name__ == "__main__":
                 dpg.add_text("Song List:", tag="songlist_title")
                 dpg.add_text("No songs added yet.", tag="songlist_content")
 
-            #Sorting
+            #Sorting (4th column)
             with dpg.child(tag="SortFunctions", width=220):
                 dpg.add_text("Sorting:")
 
